a3bf456e67a3740129340190c0908e1519a142b5
JP Rshaid <40834157+TheSirYeti@users.noreply.github.com>
2022-03-20 01:46:54 -0300

    Cambios faciles que hice en 2 segunods

--- Assets/DEMO/DemoPlayer.cs
+++ Assets/DEMO/DemoPlayer.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class DemoPlayer : MonoBehaviour
 {
     private Vector3 _vMovement;
+    public Transform direction;
     [SerializeField] private float _speed = 2;
 
     private bool _isFighting = false;
@@ -12,8 +13,11 @@ public class DemoPlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        _vMovement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        _vMovement = Input.GetAxis("Horizontal") * direction.right + Input.GetAxis("Vertical") * direction.forward;
 
         transform.position += _vMovement * _speed * Time.deltaTime;
+
+        //y si te invito a una copa y me hacerco a tu boca <3
+
     }
 }
