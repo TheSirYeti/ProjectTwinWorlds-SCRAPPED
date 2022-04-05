f0efc867cdb33d2de38994fd632afce6d82d8426
MaiineV <agustinmartinezbarrios2@hotmail.com>
2022-04-04 23:20:39 -0300

    UI Funcional

--- Assets/Playgrounds/Maiine/Scripts/UIController.cs
+++ Assets/Playgrounds/Maiine/Scripts/UIController.cs
@@ -7,14 +7,71 @@ public class UIController : MonoBehaviour
 {
     [SerializeField] private List<Image> UI;
     [SerializeField] private bool UIOn;
+
+    [SerializeField] private List<GameObject> _demonHearths;
+    [SerializeField] private List<GameObject> _angelHearths;
+
+    [SerializeField] private GameObject _demonUI;
+    [SerializeField] private GameObject _angelUI;
+
+    [SerializeField] private GameObject _demonPlayer;
+    [SerializeField] private GameObject _angelPlayer;
+
+    private bool _isDemond = true;
+    private int _uiDemondLife = 3;
+    private int _uiAngelLife = 3;
     private Color c;
 
     private void Start()
     {
         c.a = 1;
+
+        EventManager.Subscribe("OnPlayerHPUpdate", TakeUIDMG);
+        EventManager.Subscribe("OnPlayerChange", ChangeUI);
     }
 
-    void Update()
+    private void Update()
+    {
+        if (_isDemond)
+        {
+            _demonUI.transform.position = _demonPlayer.transform.position;
+        }
+        else
+        {
+            _angelUI.transform.position = _angelPlayer.transform.position;
+        }
+    }
+
+    public void ChangeUI(object[] parameters)
+    {
+        _isDemond = !_isDemond;
+        if (_isDemond)
+        {
+            _demonUI.gameObject.SetActive(true);
+            _angelUI.gameObject.SetActive(false);
+        }
+        else
+        {
+            _demonUI.gameObject.SetActive(false);
+            _angelUI.gameObject.SetActive(true);
+        }
+    }
+
+    public void TakeUIDMG(object[] parameters)
+    {
+        if((bool)parameters[0])
+        {
+            _uiDemondLife--;
+            _demonHearths[_uiDemondLife].gameObject.SetActive(false);
+        }
+        else
+        {
+            _uiAngelLife--;
+            _angelHearths[_uiAngelLife].gameObject.SetActive(false);
+        }
+    }
+
+    void FadeOut()
     {
         if (UIOn)
         {
