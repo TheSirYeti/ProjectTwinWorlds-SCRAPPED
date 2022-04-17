dcf74944443dee2573db34d3f9f7ca855605d222
JP Rshaid <40834157+TheSirYeti@users.noreply.github.com>
2022-04-17 02:29:43 -0300

    Mejora a Cambio de Mundo (Implementacion del Shader)

---
+++ Assets/0_Scripts/Worlds/WorldObject.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class WorldObject : MonoBehaviour
+{
+    public Material angelOffMaterial, demonOffMaterial;
+    public Material defaultMaterial;
+    public Collider collider;
+
+    public bool isDemon;
+    public bool isWorldActive;
+    private void Start()
+    {
+        EventManager.Subscribe("OnPlayerChange", SwapMaterial);
+        defaultMaterial = GetComponent<Renderer>().material;
+
+        if (!isDemon)
+        {
+            isWorldActive = false;
+            GetComponent<Renderer>().material = angelOffMaterial;
+            collider.enabled = false;
+        }
+        else
+        {
+            isWorldActive = true;
+            GetComponent<Renderer>().material = defaultMaterial;
+            collider.enabled = true;
+        }
+    }
+
+    public void SwapMaterial(object[] parameters)
+    {
+        GameObject myPlayer = (GameObject) parameters[0];
+        
+        if (isWorldActive)
+        {
+            if (myPlayer.layer == LayerMask.NameToLayer("DemonPlayer"))
+                GetComponent<Renderer>().material = demonOffMaterial;
+            else GetComponent<Renderer>().material = angelOffMaterial;
+
+            isWorldActive = false;
+            collider.enabled = false;
+        }
+        else
+        {
+            isWorldActive = true;
+            GetComponent<Renderer>().material = defaultMaterial;
+            collider.enabled = true;
+        }
+    }
+}
