efa2bd7c1e3c0675772640b10e0607ee78383a71
MaiineV <agustinmartinezbarrios2@hotmail.com>
2022-04-19 01:41:44 -0300

    Comodo lvl 2

--- Assets/Playgrounds/Maiine/Scripts/FloorLevelController.cs
+++ Assets/Playgrounds/Maiine/Scripts/FloorLevelController.cs
@@ -5,25 +5,28 @@ using UnityEngine;
 public class FloorLevelController : MonoBehaviour
 {
     [SerializeField] private List<GameObject> itemsLvl;
+    [SerializeField] Material BaseMat;
+    [SerializeField] Material OpacityMat;
+
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.tag == "Player")
+        if(other.gameObject.GetComponent<PlayerMovement>() != null)
         {
             foreach (var item in itemsLvl)
             {
-                item.gameObject.SetActive(false);
+                item.gameObject.GetComponent<Renderer>().material = OpacityMat;
             }
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.gameObject.tag == "Player")
+        if (other.gameObject.GetComponent<PlayerMovement>() != null)
         {
             foreach (var item in itemsLvl)
             {
-                item.gameObject.SetActive(true);
+                item.gameObject.GetComponent<Renderer>().material = BaseMat;
             }
         }
     }
