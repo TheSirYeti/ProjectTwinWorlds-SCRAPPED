594d220149324c9303048d963de4cb885e3e6c9d
JP Rshaid <40834157+TheSirYeti@users.noreply.github.com>
2022-04-30 22:54:23 -0300

    Asi es, mas bugfixes

--- Assets/0_Scripts/Player/PlayerFloorTrigger.cs
+++ Assets/0_Scripts/Player/PlayerFloorTrigger.cs
@@ -1,11 +1,18 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Net.Sockets;
 using UnityEngine;
 
 public class PlayerFloorTrigger : MonoBehaviour
 {
     public PlayerMovement angelMovement, demonMovement;
+    public Transform reference;
+
+    private void Update()
+    {
+        transform.position = reference.position;
+    }
 
     private void OnTriggerEnter(Collider other)
     {
