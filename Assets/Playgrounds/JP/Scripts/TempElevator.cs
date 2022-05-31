098b406703a2ee0a558def6aaa24729b47f6533a
MaiineV <agustinmartinezbarrios2@hotmail.com>
2022-05-31 05:38:53 -0300

    """""""""""""""""""FIX DEL ASCENSOR"""""""""""""""""""""""""""""""

--- Assets/Playgrounds/JP/Scripts/TempElevator.cs
+++ Assets/Playgrounds/JP/Scripts/TempElevator.cs
@@ -10,6 +10,9 @@ public class TempElevator : MonoBehaviour
     public Transform elevator;
     public float speed, minDistance;
 
+    //TEMPORAL!!!!!!!!!!!!
+    List<Transform> transformIn = new List<Transform>();
+
     private bool isStalling;
     private int currentWaypoint;
     private void Update()
@@ -18,6 +21,14 @@ public class TempElevator : MonoBehaviour
         {
             Vector3 distance = (elevatorPoints[currentWaypoint].position - elevator.position).normalized;
             elevator.position += distance * speed * Time.deltaTime;
+
+            if (transformIn.Count > 0)
+            {
+                foreach (Transform transformFor in transformIn)
+                {
+                    transformFor.position += distance * speed * Time.deltaTime;
+                }
+            }
 
             if (Vector3.Distance(elevator.position, elevatorPoints[currentWaypoint].position) <= minDistance)
             {
@@ -44,5 +55,29 @@ public class TempElevator : MonoBehaviour
             elevatorPoints.Reverse();
             currentWaypoint = 0;
         }
+    }
+
+
+    ///TEMPORAL!!!!!!!!!!!
+    private void OnTriggerEnter(Collider other)
+    {
+        Player actualPlayer = other.gameObject.GetComponent<Player>();
+        IWeaponInteractable objectInteract = other.gameObject.GetComponent<IWeaponInteractable>();
+
+        if (actualPlayer != null)
+            transformIn.Add(actualPlayer.transform);
+        else if (objectInteract != null)
+            transformIn.Add(objectInteract.Inter_GetGameObject().transform);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Player actualPlayer = other.gameObject.GetComponent<Player>();
+        IWeaponInteractable objectInteract = other.gameObject.GetComponent<IWeaponInteractable>();
+
+        if (actualPlayer != null)
+            transformIn.Remove(actualPlayer.transform);
+        else if (objectInteract != null)
+            transformIn.Remove(objectInteract.Inter_GetGameObject().transform);
     }
 }
